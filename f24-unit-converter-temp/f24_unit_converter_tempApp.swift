4178a7751b2cd7eaa8a7f49a51de941f90341fcf
Brenton Lian <73314123+brentonlian@users.noreply.github.com>
2024-10-08 00:16:41 -0400

    Add files via upload due to GitHub issues

---
+++ f24-unit-converter-temp/f24_unit_converter_tempApp.swift
@@ -0,0 +1,17 @@
+//
+//  f24_unit_converter_tempApp.swift
+//  f24-unit-converter-temp
+//
+//  Created by Brenton on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct f24_unit_converter_tempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
